f96d867a993b48f354c985c24a092d51c1d381b1
Joel Rojas Soliz <joel.rojas.soliz@gmail.com>
2018-04-25 03:27:00 -0400

    Modify photo apis.

--- api/photo/photo.controller.js
+++ api/photo/photo.controller.js
@@ -1,17 +1,32 @@
 
 import {Photo} from './photo.model';
 import {photoList, addPhoto} from '../aws';
+import { resolve } from 'path';
 
 export function list (req, res) {
-    const listPhotoDBProm = Photo.findAll();
-    const listPhotoImgProm = photoList();
-    Promise.all([listPhotoDBProm, listPhotoImgProm])
-        .then(dataSet => {
-            const photoDBSet = dataSet[0];
-            const photoImgSet = dataSet[1];
-            return res.status(200).json({photoDBSet, photoImgSet});
-        }).catch(err => {
-            return res.status(503).json({message: `Couldn't get photo list from server: ${JSON.stringify(err)}`});
-        });
+    Photo.findAll().then(response => {
+        const {data} = response;
+        return res.status(200).json({success: true, data});
+    }).catch(err => {
+        const errMsg = typeof err === 'object' ? JSON.stringify(err) : err;        
+        return res.status(503).json({success: false, data: `Couldn't retrieve list of photos: ${errMsg}`});
+    });
 }
-export function add (req, res) {}
+export function add (req, res) {
+    const bodyData = req.body;
+    const {name, description, file} = bodyData;
+    addPhoto(file).then(response => {
+        const data = response.data;
+        console.log(data);  
+        return Photo.create({
+            name,
+            description,
+            link: data.Location
+        });
+    }).then(photo => {
+        return res.status(200).json({success: true, data: 'Photo successfully created.'});
+    }).catch(err => {
+        const errMsg = typeof err === 'object' ? JSON.stringify(err) : err;        
+        return res.status(503).json({success: false, data: `Couldn't create photo: ${errMsg}`});
+    });
+}
